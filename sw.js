6d8f90d86e1effe2b41aae45123385f2844d4ca6
coke-ros3 <rosaleslanderosjorgeantonio@gmail.com>
2026-07-31 22:37:50 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = 'bomberos-cad-v2';
+// Aumentamos a la versión 3 para obligar al navegador a actualizar el diseño
+const CACHE_NAME = 'bomberos-cad-v3';
 const urlsToCache = [
   './',
   './index.html',
@@ -16,6 +17,21 @@ const urlsToCache = [
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Este bloque nuevo borra por completo el CSS antiguo y roto
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
 
